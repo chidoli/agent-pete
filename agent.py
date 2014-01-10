a33ad414f59ddf9c253e4739951907f4ff6ec703
Chihyun Lim <chidoli37@gmail.com>
2014-01-10 05:12:02 -0500

    multiprocessing

--- agent.py
+++ agent.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 
 import mechanize
-import threading
 import time
 import copy
 from HTMLParser import HTMLParser
+from multiprocessing.pool import ThreadPool
 
 
 class MyHTMLParser(HTMLParser):
@@ -71,8 +71,6 @@ def getAttr(html):
     return attrs
     
 
-seat_all = []
-
 def getSeats(html):
     seats =[]
     html = html.split('\n')
@@ -84,165 +82,115 @@ def getSeats(html):
     return seats[3] # third entry of this page gives the num of available seats
  
 
+def fetchLink(br, link):
+    br.follow_link(link)
+    html = br.response().read()
 
-# Link Thread returns the following thread
-# [ <course section>, <course crn> , <seat>, <course name> ]
-class linkThread(threading.Thread):
-  def __init__(self, index, br, link):
-      threading.Thread.__init__(self)
-      self.index = index
-      self.br = br
-      self.link = link
+    seat = []
 
-  def run(self):
-      self.br.follow_link(self.link)
-      html = self.br.response().read()
+    # get section number here
+    course_raw = link.text
+    course_split = course_raw.split(" -");
+    course_section = course_split[3]
+    course_section = course_section[1:len(course_section)]
+    seat.append(course_section)
 
-      #print "thread " + str(self.index) + " launched.."
-      global seat_all
-      seat_current = []
-      
-      # get section number here
-      course_raw = self.link.text
-      course_split = course_raw.split(" -");
-      course_section = course_split[3]
-      course_section = course_section[1:len(course_section)]
-      seat_current.append(course_section)
+    # get crn
+    course_crn = course_split[1];
+    seat.append(course_crn)
 
-      # get crn
-      course_crn = course_split[1];
-      seat_current.append(course_crn)
+    # get seat info here
+    seat.append(getSeats(html))
 
-      # get seat info here
-      seat_current.append(getSeats(html))
+    # get course name
+    seat.append( course_split[0] )
 
-      # get course name
-      seat_current.append( course_split[0] )
-
-      # commit
-      seat_all[self.index] = seat_current
-      html = self.br.response().read()
+    return seat
 
 def normalizeInput(coursename):
-    # conver to lower case and classify
-    coursename = coursename.upper();        # convert to lower
-    coursename = coursename.replace(' ','') # remove spaces
+    coursename = coursename.upper();
+    coursename = coursename.replace(' ','') 
 
-    # find the division between <coursedept><coursenum>
-    index = 0
-    for char in coursename:
-        if char.isdigit():
-            break 
-        index += 1
+    subj = None
+    num = None
+    for i, c in enumerate(coursename):
+        if c.isdigit():
+            subj = coursename[:i]
+            num = coursename[i:]
+            break
 
-    # check 1: course number must be 3 or 5
-    if ( (coursename.__len__() - index) != 3 ) and ( (coursename.__len__() - index) != 5 ) :
-        return None
+    if len(num) == 3:
+        num += '00'
+    if not len(num) == 5:
+        raise Exception('Invalid course number: %s' % (subj+num))
 
-    # check 2: dept name must be less than 4 chars long
-    if index > 4 :
-        return None
+    if len(subj) >= 4:
+        raise Exception('Invalid course subject: %s' % (subj+num))
 
-    # we should add 00 at the end if the course number length == 3
-    if ( (coursename.__len__() - index) == 3): 
-        coursename = coursename + "00"
+    if subj == 'BIO':
+        subj = 'BIOL'
 
-        # common mistakes 1: people write bio instead of biol
-        if coursename[0] == "BIO":
-            newInput = ("BIOL", coursename[0])
-            coursename = newInput
-
-    return (coursename[0:index], coursename[index:coursename.__len__()])
+    return (subj, num)
 
 
-def crawlPurdue(coursename):
-    """
-    Returns a dictionary of all sections with following information:
-    section number, section type, section time, section days
-    section availability, section instructor
-    """
-
-    # Normalize Input
-    coursename = normalizeInput(coursename)
-
-    if coursename == None:
-      #print "err: bad coursename"
-      return None
+def getCourseInfo(coursename):
+    c_subj, c_num = normalizeInput(coursename)
 
     br = mechanize.Browser()
-    # set some headers
     br.set_handle_robots(False)
     br.addheaders = [('User-agent', 'Firefox')]
 
     # open schedule
     br.open("http://wl.mypurdue.purdue.edu/schedule")
-  
-    # select form
     br.form = list( br.forms() )[0]
     # 201410 = fall 2014, 201420 = Spring, 201430 = summer
     br['p_term'] = ['201420']
-  
-    # submit the form
     br.submit()         
     
-    #print br.response().read()
-  
     # Now we are in course selection mode
     br.form = list( br.forms() )[0]
-    try :
-        for control in br.form.controls:
-            if control.name == 'sel_subj' and control.type == 'select':
-              control.value = [coursename[0]]
-            if control.name == 'sel_crse' and control.type == 'text':
-              control.value = coursename[1]
-    except:
-        import sys
-        e = sys.exc_info()[0] 
-        return None
-
+    for control in br.form.controls:
+        if control.name == 'sel_subj' and control.type == 'select':
+            control.value = [c_subj]
+        if control.name == 'sel_crse' and control.type == 'text':
+            control.value = c_num
     br.submit()
   
-  
-    # We are now in Course Schedule Listing Page
+    # Course Schedule Listing Page
     html = br.response().read() 
-    section_all = getAttr(html)
+    sections = getAttr(html)
    
-    #print attr_all
     # now we need to find the link to the course
     links = []
-  
     for link in br.links():
         if link.text is None:
             continue
-        if coursename[0] in link.text and coursename[1] in link.text:
+        if c_subj in link.text and c_num in link.text:
             # all the follwing links must be of some value
             links.append(link)
   
     if not links:
-        #print "course not found"
-        return None
+        raise Exception('Course %s is not found' % (c_subj, c_num))
   
     # visit links and get seats/waitlist seats
-    global seat_all
-    seat_all_len = len(links)
-    seat_all = [None for x in xrange(seat_all_len)]
-    threads = [None for x in xrange(seat_all_len)]
-
-    i = 0
+    asyncs = []
+    pool = ThreadPool()
     for link in links:
-        threads[i] = linkThread( i, copy.copy(br), link  )
-        threads[i].start()
-        i += 1
-    
-    # wait for all threads to complete
-    for thread in threads:
-        thread.join()
+        a = pool.apply_async(fetchLink, [copy.copy(br), link])
+        asyncs.append(a)
+
+    seats = []
+    for a in asyncs:
+        v = a.get()
+        if v:
+            seats.append(v)
+        asyncs.remove(a)
 
     # merge section dic with seat_all into ret_all
     ret_all = {
-        'Course': coursename[0] + coursename[1] + " " + seat_all[0][3],
+        'Course': c_subj + c_num + ' ' + seats[0][3],
     }
-    for seat, section in zip(seat_all, section_all):
+    for seat, section in zip(seats, sections):
         stype = section['stype'][0]
         if stype not in ret_all:
             ret_all[stype] = {}
@@ -251,14 +199,10 @@ def crawlPurdue(coursename):
             'sec': seat[0],
             'inst': section['inst'], 
             'days': section['days'],  
-            'seat': seat[2]
+            'seats': seat[2],
         }
     return ret_all
 
-def getCourseInfo(arg):
-    ret = crawlPurdue(arg)
-    return ret
-  
   
 if __name__ == '__main__':
    start = time.clock()
