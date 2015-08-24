0aca7ddcbce2454aa11f8eb1c2944ccfefdcdb48
Chi Hyun Lim <chidoli37@gmail.com>
2015-08-24 12:01:40 -0400

    Uses sendmail command

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import smtplib
 from email.mime.text import MIMEText
 import json
 
@@ -6,19 +5,22 @@ from agent import getCourseInfo
 from config import conf, load_conf
 from util import now_str
 
+from subprocess import Popen, PIPE
+
 
 def sendmail(sender, receiver, subject, content):
+    cmd = ['sendmail', '-t']
+    p = Popen(cmd, stdin=PIPE)
+
     msg = MIMEText(content, 'html')
     msg['From'] = sender
     msg['To'] = receiver
     msg['Subject'] = subject
+    msg['Content-Type'] = 'text/html'
 
-    smtp = conf['smtp']
-    s = smtplib.SMTP(smtp['server'], smtp['port'])
-    s.starttls()
-    s.login(smtp['id'], smtp['pw'])
-    s.sendmail(sender, [receiver], msg.as_string())
-    s.quit()
+    p.stdin.write(msg.as_string())
+    res = p.communicate()[0]
+    # print res
 
 def trsForSection(crstype, crn, code, seats, m, first_m):
     body = ''
@@ -43,8 +45,6 @@ def main():
         load_conf(sys.argv[1])
     else:
         raise Exception('Configuration file needed. (ex. default.conf)')
-    if conf['smtp']['pw'] == '':
-        raise Exception('SMTP password is not configured')
 
     sender = conf['sys']['sender']
 
@@ -69,8 +69,8 @@ def main():
                 for m in sec['meetings']:
                     body += trsForSection(k, crn, code, seats, m, first_m)
                     if seats > 0:
-                        if 'crn' in req:
-                            for c in req['crn']:
+                        if 'crns' in req:
+                            for c in req['crns']:
                                 valid = crn in c
                                 if valid:
                                     break
@@ -80,19 +80,26 @@ def main():
                             availables.append([k, crn, code, seats, m, first_m])
                     first_m = False
         
-        if 'crn' in req:
-            for crns in req['crn']:
+        if 'crns' in req:
+            for crns in req['crns']:
                 all_in = True
-                avaCrns= [x[2] for x in availables]
+                avaCrns = [x[2] for x in availables]
                 for crn in crns:
                     if crn not in avaCrns:
                         all_in = False
                         break
                 if not all_in:
+                    idxs = []
                     for crn in crns:
                         if crn in avaCrns:
-                            availables.pop(avaCrns.index(crn))
-
+                            idxs.append(avaCrns.index(crn))
+                    
+                    new_availables = []
+                    for i in xrange(len(availables)):
+                        if i not in idxs:
+                            new_availables.append(availables.get(i))
+                    availables = new_availables
+                        
         if len(availables) > 0:
             title = '<h4>%s</h4>\n' % info['Course']
             body = '%s\n<table>\n%s</table>\n' % (title, body)
